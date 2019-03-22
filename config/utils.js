bf53de1e79be67e3155f6ef51a47670bcd64ef7b
18348086338 <xiaohan0k@163.com>
2019-03-22 14:05:22 +0800

    新增目录结构

---
+++ config/utils.js
@@ -0,0 +1,10 @@
+//dev开发模式配置清单
+const dbConfig = {
+  host:'123.207.110.222',
+  port:3306,
+  user:'bibiziyuan',
+  password:'HpSJY8dXz7rCZX4y',
+  database:'bibiziyuan',
+  charset:'utf8'
+};
+module.exports = dbConfig;
